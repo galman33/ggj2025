6ee71a14f9233823226784ace8a26511468579b0
Gal Pasternak <galman33@gmail.com>
2025-01-23 22:08:55 +0200

    WIP

---
+++ back_bubbles.lua
@@ -0,0 +1,26 @@
+back_bubbles = {}
+next_back_bubble_time = 0
+
+function update_back_bubbles()
+    next_back_bubble_time -= 1
+
+    if next_back_bubble_time <= 0 then
+        add(back_bubbles, { x = 64 + rnd(20) - 10, y = 128, vx = rnd(2) - 1, vy = -2 })
+        next_back_bubble_time = 10
+    end
+
+    for bubble in all(back_bubbles) do
+        bubble.x += bubble.vx
+        bubble.y += bubble.vy
+
+        if bubble.y < 0 then
+            del(back_bubbles, bubble)
+        end
+    end
+end
+
+function draw_back_bubbles()
+    for bubble in all(back_bubbles) do
+        circfill(bubble.x, bubble.y, 2, 7)
+    end
+end
--- main.lua
+++ main.lua
@@ -1,10 +1,57 @@
+bubble1_y = 128
+bubble2_y = 128 * 4
+
 function _init()
 end
 
 function _update()
+    update_back_bubbles()
+
+    if bubble1_y > 30 then
+        bubble1_y -= 10
+    end
+
+    if bubble2_y > 55 then
+        bubble2_y -= 10
+    end
 end
 
 function _draw()
-    cls(12)
-    print("hello ggj 2025", 35 + sin(t()) * 10, 50, rnd(15))
+    cls(130)
+
+    draw_back_bubbles()
+
+    local y = sin(t() * 1) * 2
+    draw_text_bubble("hello", 0.5, 20, bubble1_y + y, false)
+    --draw_text_bubble("bye", 2, 20, bubble2_y + y, true)
+    draw_text_bubble("12345678901234567890", 2, 20, bubble2_y + y, true)
+
+    print("hello ggj 2025", 35 + sin(t()) * 10, 5, rnd(15))
+end
+
+function draw_text_bubble(txt, start_time, x, y, side)
+    local bubble_w = 80
+
+    local c = 7
+    if (side) c = 11
+    circfill(x + 5, y + 5, 5, c)
+    circfill(x + 5 + bubble_w, y + 5, 5, c)
+    rectfill(x + 5, y, x + 5 + bubble_w, y + 10, c)
+
+    local txt_speed = 15
+    local n = max(min(flr((t() - start_time) * txt_speed), #txt), 0)
+    local txt_sub = sub(txt, 1, n)
+    print(txt_sub, x + 5, y + 3, 0)
+
+    if side then
+        local tip_x = x + bubble_w - 2
+        line(tip_x + 5, y + 11, tip_x + 8, y + 11, c)
+        line(tip_x + 7, y + 12, tip_x + 9, y + 12, c)
+        line(tip_x + 8, y + 13, tip_x + 10, y + 13, c)
+    else
+        local tip_x = x - 2
+        line(tip_x + 5, y + 11, tip_x + 8, y + 11, c)
+        line(tip_x + 4, y + 12, tip_x + 6, y + 12, c)
+        line(tip_x + 3, y + 13, tip_x + 5, y + 13, c)
+    end
 end
